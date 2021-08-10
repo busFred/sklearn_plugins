9b5ad6f487e0f2046021fffba572bac4db9e3e98
busFred <32878707+busFred@users.noreply.github.com>
2021-08-10 15:07:29 -0400

    add init_alpha_matrix

--- src/sklearn_plugins/rvm/rvm.py
+++ src/sklearn_plugins/rvm/rvm.py
@@ -2,6 +2,7 @@ from abc import ABC, abstractmethod
 from typing import Callable, Dict, Optional, Tuple, Union
 
 import numpy as np
+from numpy.core.fromnumeric import shape
 from numpy.random import RandomState
 from sklearn.base import BaseEstimator
 from sklearn.metrics.pairwise import (linear_kernel, rbf_kernel,
@@ -9,6 +10,19 @@ from sklearn.metrics.pairwise import (linear_kernel, rbf_kernel,
 
 
 class BaseRVM(BaseEstimator, ABC):
+    """BaseRVM
+
+    Args:
+        kernel (Union[str, Callable[[np.ndarray, np.ndarray], np.ndarray]], optional): The kernel function to be used. Defaults to 'rbf'.
+        degree (int, optional): Degree of the polynomial kernel function (‘poly’). Ignored by all other kernels. Defaults to 3.
+        gamma (Union[str, float], optional): Kernel coefficient for ‘rbf’, ‘poly’ and ‘sigmoid’. Defaults to 'scale'.
+        coef0 (float, optional): Independent term in kernel function. It is only significant in ‘poly’ and ‘sigmoid’. Defaults to 0.0.
+        include_bias (bool, optional): include bias in the polynomial. Defaults to True.
+        tol (float, optional): tolerance for stopping criterion. Defaults to 1e-3.
+        verbose (bool, optional): [description]. Defaults to False.
+        max_iter (int, optional): [description]. Defaults to -1.
+        random_state (Optional[Union[int, RandomState]], optional): [description]. Defaults to None.
+    """
     kernel: Union[str, Callable[[np.ndarray, np.ndarray], np.ndarray]]
     degree: int
     gamma: Union[str, float]
@@ -51,6 +65,12 @@ class BaseRVM(BaseEstimator, ABC):
             X: np.ndarray,
             y: np.ndarray,
             sample_weight: Optional[np.ndarray] = None) -> "BaseRVM":
+        phi: np.ndarray = self._apply_kernel_func(X=X,
+                                                  Y=X)  # the design matrix
+        beta: np.ndarray = self._compute_beta_matrix(phi=phi, target=y)
+        alpha_matrix: np.ndarray = self._init_alpha_matrix(phi=phi,
+                                                           target=y,
+                                                           beta=beta)
         return self
 
     @abstractmethod
@@ -109,8 +129,8 @@ class BaseRVM(BaseEstimator, ABC):
                     "Argument 'gamma' should be of type str or float, but 'gamma' is of type {} and has value {}",
                     type(self.gamma), self.gamma))
 
-    def _compute_kernel_func(self, X: np.ndarray, Y: np.ndarray) -> np.ndarray:
-        """Compute kernel matrix.
+    def _apply_kernel_func(self, X: np.ndarray, Y: np.ndarray) -> np.ndarray:
+        """Apply kernel function.
 
         Args:
             X (np.ndarray): (n_samples_X, n_features)
@@ -165,3 +185,33 @@ class BaseRVM(BaseEstimator, ABC):
         proj_norm: np.ndarray = np.divide(proj, phi_norm)
         idx: int = np.argmax(proj_norm)
         return idx, phi[:, idx]
+
+    @abstractmethod
+    def _compute_beta_matrix(self, phi: np.ndarray,
+                             target: np.ndarray) -> np.ndarray:
+        pass
+
+    def _init_alpha_matrix(self, phi: np.ndarray, target: np.ndarray,
+                           beta_matrix: np.ndarray) -> np.ndarray:
+        """Initialize alpha matrix
+
+        Args:
+            phi (np.ndarray): (n_samples, n_basis_vectors) or (N, M) in Tipping 2003.
+            target (np.ndarray): (n_samples) the target vector.
+            beta_matrix (np.ndarray): (n_samples, n_samples) or (N, N) the beta matrix with beta_i on the diagonal.
+
+        Returns:
+            alpha_matrix (np.ndarray): (n_basis_vectors, n_basis_vectors) or (M, M) in Tipping 2003. the alpha matrix
+        """
+        # beta_i = 1 / sigma_i**2
+        idx, basis_vector = self._select_basis_vector(phi=phi, target=target)
+        alpha_vector: np.ndarray = np.full(shape=(phi.shape[1]),
+                                           fill_value=np.inf)
+        basis_norm: float = np.linalg.norm(basis_vector, axis=0)
+        beta: float = beta_matrix[idx, idx]
+        alpha_i: float = basis_norm**2 / (
+            (np.matmul(x1=basis_vector.T, x2=target)**2 / basis_norm**2) -
+            (1 / beta))
+        alpha_vector[idx] = alpha_i
+        alpha_matrix: np.ndarray = np.diag(v=alpha_vector)
+        return alpha_matrix

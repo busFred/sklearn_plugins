ef73457b21718f52f75a5bd7f2cfe1eb8c13e68d
busFred <32878707+busFred@users.noreply.github.com>
2021-08-12 12:54:18 -0400

    add compute_phi_matrix

--- src/sklearn_plugins/rvm/rvm.py
+++ src/sklearn_plugins/rvm/rvm.py
@@ -3,41 +3,75 @@ from functools import partial
 from typing import Callable, Dict, Optional, Tuple, Union
 
 import numpy as np
-from numpy.random import RandomState
+from numpy.core.fromnumeric import shape
 from sklearn.base import BaseEstimator
 from sklearn.metrics.pairwise import rbf_kernel
 
 
 class BaseRVM(BaseEstimator, ABC):
-    kernel_func: Callable[[np.ndarray, np.ndarray], np.ndarray]
-    y_var: float
-    update_y_var: bool
-    include_bias: bool
-    tol: float
-    max_iter: Optional[int]
+    _kernel_func: Callable[[np.ndarray, np.ndarray], np.ndarray]
+    _include_bias: bool
+    _tol: float
+    _max_iter: Union[int, None]
+
+    _relevance_vectors_: Union[np.ndarray, None]
 
     def __init__(self,
                  kernel_func: Callable[[np.ndarray, np.ndarray],
                                        np.ndarray] = partial(rbf_kernel,
                                                              gamma=None),
-                 y_var: float = 1e-6,
-                 update_y_var: bool = False,
                  include_bias: bool = True,
                  tol: float = 1e-3,
                  max_iter: Optional[int] = None) -> None:
         super().__init__()
-        self.kernel_func = kernel_func
-        self.y_var = y_var
-        self.update_y_var = update_y_var
-        self.include_bias = include_bias
-        self.tol = tol
-        self.max_iter = max_iter
+        self._kernel_func = kernel_func
+        self._include_bias = include_bias
+        self._tol = tol
+        self._max_iter = max_iter
 
     def fit(self, X: np.ndarray, y: np.ndarray) -> "BaseRVM":
-        phi_matrix: np.ndarray = self.kernel_func(X, X)
-        
+        phi_matrix: np.ndarray = self._compute_phi_matrix(X, X)
+
         return self
 
     @abstractmethod
     def predict(self, X: np.ndarray) -> np.ndarray:
         pass
+
+    def _compute_phi_matrix(self, X: np.ndarray,
+                            X_prime: np.ndarray) -> np.ndarray:
+        phi_matrix: np.ndarray = self._kernel_func(X, X_prime)
+        if self._include_bias == True:
+            n_samples: int = phi_matrix.shape[0]
+            phi_matrix = np.append(phi_matrix,
+                                   values=np.ones(shape=(n_samples, 1)),
+                                   axis=1)
+        return phi_matrix
+
+    @abstractmethod
+    def _init_beta_matrix(self) -> np.ndarray:
+        pass
+
+    # public properties
+    @property
+    def kernel_func(self) -> Callable[[np.ndarray, np.ndarray], np.ndarray]:
+        return self._kernel_func
+
+    @property
+    def include_bias(self) -> bool:
+        return self._include_bias
+
+    @property
+    def tol(self) -> float:
+        return self._tol
+
+    @property
+    def max_iter(self) -> Union[int, None]:
+        return self._max_iter
+
+    @property
+    def relevance_vectors_(self) -> np.ndarray:
+        if self._relevance_vectors_ is not None:
+            return self._relevance_vectors_
+        else:
+            raise ValueError("self._relevance_vectors_ is None")

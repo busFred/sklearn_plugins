b8309094782896a7766db02c5d81fe249ec042ae
busFred <32878707+busFred@users.noreply.github.com>
2021-08-14 12:55:56 -0400

    modify test_rvr

--- test/sklearn_plugins/rvm/test_rvr.py
+++ test/sklearn_plugins/rvm/test_rvr.py
@@ -5,7 +5,7 @@ from sklearn_plugins.rvm.rvr import RVR
 
 #%%
 data, target = datasets.load_diabetes(return_X_y=True)
-X: np.ndarray = np.expand_dims(data[:, 2],axis=1)
+X: np.ndarray = np.expand_dims(data[:, 2], axis=1)
 y: np.ndarray = target
 
 #%%
@@ -15,5 +15,14 @@ X_val: np.ndarray = X[-20:]
 y_val: np.ndarray = y[-20:]
 
 #%%
-rvr: RVR = RVR()
+rvr: RVR = RVR(max_iter=10000, verbose=True)
 rvr.fit(X=X_train, y=y_train)
+# %%
+y_train_pred: np.ndarray = rvr.predict(X_train)
+# %%
+print(np.average(np.abs(y_train_pred - y_train)))
+# %%
+import matplotlib.pyplot as plt
+plt.scatter(X_train, y_train)
+plt.scatter(X_train, y_train_pred)
+# %%

fix return variance

src/sklearn_plugins/rvm/rvr.py
@@ -45,7 +45,7 @@ class RVR(BaseRVM):
         if return_var == False:
             return y
         y_var: np.ndarray = np.full_like(y, self._y_var_)
-        y_var = y_var + phi_matrix.T @ self._sigma_matrix @ phi_matrix
+        y_var = y_var + phi_matrix @ self._sigma_matrix @ phi_matrix.T
         return y, y_var
 
     @overrides
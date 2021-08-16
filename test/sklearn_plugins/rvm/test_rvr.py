70687b853a44f0e42f7f0e312c8106075cebe901
busFred <32878707+busFred@users.noreply.github.com>
2021-08-16 19:50:27 -0400

    update test_rvr with sinc

--- test/sklearn_plugins/rvm/test_rvr.py
+++ test/sklearn_plugins/rvm/test_rvr.py
@@ -1,34 +1,45 @@
 #%%
 import matplotlib.pyplot as plt
 import numpy as np
-from sklearn import datasets
+from sklearn.metrics import mean_squared_error
+from sklearn.model_selection import train_test_split
 from sklearn_plugins.rvm.rvr import RVR
 
 #%%
-data, target = datasets.load_diabetes(return_X_y=True)
-X: np.ndarray = np.expand_dims(data[:, 2], axis=1)
-y: np.ndarray = target
+N_SAMPLES: int = 5000
 
 #%%
-X_train: np.ndarray = X[:-20]
-y_train: np.ndarray = y[:-20]
-X_val: np.ndarray = X[-20:]
-y_val: np.ndarray = y[-20:]
+np.random.seed(0)
+Xc = np.ones([N_SAMPLES, 1])
+Xc[:, 0] = np.linspace(-5, 5, N_SAMPLES)
+Yc = 10 * np.sinc(Xc[:, 0]) + np.random.normal(0, 1, N_SAMPLES)
+X_train, X_val, y_train, y_val = train_test_split(Xc,
+                                                  Yc,
+                                                  test_size=0.5,
+                                                  random_state=0)
 
 #%%
-rvr: RVR = RVR(max_iter=10000, verbose=True)
+rvr: RVR = RVR(max_iter=300, verbose=True)
 rvr.fit(X=X_train, y=y_train)
-# %%
-y_train_pred: np.ndarray = rvr.predict(X_train)
-# %%
-print(np.average(np.abs(y_train_pred - y_train)))
-# %%
-plt.scatter(X_train, y_train)
-plt.scatter(X_train, y_train_pred)
-# %%
-y_val_pred: np.ndarray = rvr.predict(X_val)
-# %%
-print(np.average(np.abs(y_val_pred - y_val)))
 #%%
+pred_train, train_var = rvr.predict(X=X_train, return_var=True)
+train_std = np.sqrt(train_var)
+mse_train: float = mean_squared_error(y_true=y_train, y_pred=pred_train)
+print(str.format("mse_train: {}", mse_train))
+#%%
+pred_val, val_var = rvr.predict(X=X_val, return_var=True)
+val_std: np.ndarray = np.sqrt(val_var)
+mse_val: float = mean_squared_error(y_true=y_val, y_pred=pred_train)
+print(str.format("mse_val: {}", mse_val))
+#%%
+plt.plot()
+plt.scatter(X_train, y_train)
+plt.scatter(X_train, pred_train)
+plt.scatter(X_train, pred_train+train_std)
+plt.scatter(X_train, pred_train-train_std)
+#%%
+plt.plot()
 plt.scatter(X_val, y_val)
-plt.scatter(X_val, y_val_pred)
+plt.scatter(X_val, pred_val)
+plt.scatter(X_val, pred_val+val_std)
+plt.scatter(X_val, pred_val-val_std)

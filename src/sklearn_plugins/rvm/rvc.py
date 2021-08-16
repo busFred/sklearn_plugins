f081031c48a3db610fe908377e2eb08fc7c13050
busFred <32878707+busFred@users.noreply.github.com>
2021-08-16 13:13:21 -0400

    class stub for RVC

--- src/sklearn_plugins/rvm/rvc.py
+++ src/sklearn_plugins/rvm/rvc.py
@@ -1,13 +1,35 @@
 from functools import partial
-from typing import Callable, Optional
+from typing import Callable, Optional, Union
 
 import numpy as np
+from sklearn.base import BaseEstimator, ClassifierMixin
 from sklearn.metrics.pairwise import rbf_kernel
 
 from .rvm import BaseRVM
 
 
-class RVC(BaseRVM):
+class RVC(ClassifierMixin, BaseEstimator):
+    class _BinaryRVC(BaseRVM):
+        def __init__(self, kernel_func: Callable[[np.ndarray, np.ndarray],
+                                                 np.ndarray],
+                     include_bias: bool, tol: float, max_iter: Optional[int],
+                     verbose: bool) -> None:
+            super().__init__(kernel_func=kernel_func,
+                             include_bias=include_bias,
+                             tol=tol,
+                             max_iter=max_iter,
+                             verbose=verbose)
+
+    _kernel_func: Callable[[np.ndarray, np.ndarray], np.ndarray]
+    _include_bias: bool
+    _tol: float
+    _max_iter: Union[int, None]
+    _verbose: bool
+
+    _relevance_vectors_: Union[np.ndarray, None]  # aka self._X_prime
+    _weight_posterior_mean_: Union[np.ndarray, None]  # aka self._mu
+    _weight_posterior_cov_: Union[np.ndarray, None]  # aka self._sigma
+
     def __init__(self,
                  kernel_func: Callable[[np.ndarray, np.ndarray],
                                        np.ndarray] = partial(rbf_kernel,
@@ -16,10 +38,54 @@ class RVC(BaseRVM):
                  tol: float = 1e-3,
                  max_iter: Optional[int] = None,
                  verbose: bool = True) -> None:
-        super().__init__(kernel_func=kernel_func,
-                         include_bias=include_bias,
-                         tol=tol,
-                         max_iter=max_iter,
-                         verbose=verbose)
+        super().__init__()
+        self._kernel_func = kernel_func
+        self._include_bias = include_bias
+        self._tol = tol
+        self._max_iter = max_iter
+        self._verbose = verbose
 
-    pass
+        self._relevance_vectors_ = None
+        self._weight_posterior_mean_ = None
+        self._weight_posterior_cov_ = None
+
+    @property
+    def kernel_func(self) -> Callable[[np.ndarray, np.ndarray], np.ndarray]:
+        return self._kernel_func
+
+    @property
+    def include_bias(self) -> bool:
+        return self._include_bias
+
+    @property
+    def tol(self) -> float:
+        return self._tol
+
+    @property
+    def max_iter(self) -> Union[int, None]:
+        return self._max_iter
+
+    @property
+    def verbose(self):
+        return self._verbose
+
+    @property
+    def relevance_vectors_(self) -> np.ndarray:
+        if self._relevance_vectors_ is not None:
+            return self._relevance_vectors_.copy()
+        else:
+            raise ValueError("self._relevance_vectors_ is None")
+
+    @property
+    def weight_posterior_mean_(self) -> np.ndarray:
+        if self._weight_posterior_mean_ is not None:
+            return self._weight_posterior_mean_.copy()
+        else:
+            raise ValueError("self._weight_posterior_mean_ is None")
+
+    @property
+    def weight_posterior_cov_(self):
+        if self._weight_posterior_cov_ is not None:
+            return self._weight_posterior_cov_.copy()
+        else:
+            raise ValueError("self._weight_posterior_cov_ is None")

605245af91c57fa438bccc824954bf84d1fd8f03
busFred <32878707+busFred@users.noreply.github.com>
2021-08-17 14:21:40 -0400

    move _BinaryRVC as inner class of RVC

--- src/sklearn_plugins/rvm/rvc.py
+++ src/sklearn_plugins/rvm/rvc.py
@@ -1,15 +1,134 @@
 from functools import partial
-from typing import Callable, List, Optional, Union
+from typing import Callable, List, Optional, Tuple, Union
 
 import numpy as np
 from overrides.overrides import overrides
+from scipy.special import expit as sigmoid
 from sklearn.base import BaseEstimator, ClassifierMixin
 from sklearn.metrics.pairwise import rbf_kernel
 
-from ._binary_rvc import _BinaryRVC
+from .rvm import BaseRVM
 
 
 class RVC(ClassifierMixin, BaseEstimator):
+    class __BinaryRVC(BaseRVM):
+        def __init__(self, kernel_func: Callable[[np.ndarray, np.ndarray],
+                                                 np.ndarray],
+                     include_bias: bool, tol: float, max_iter: Optional[int],
+                     verbose: bool) -> None:
+            super().__init__(kernel_func=kernel_func,
+                             include_bias=include_bias,
+                             tol=tol,
+                             max_iter=max_iter,
+                             verbose=verbose)
+
+        @overrides
+        def predict(self, X: np.ndarray) -> np.ndarray:
+            phi_matrix: np.ndarray = self._compute_phi_matrix(
+                X=X, X_prime=self._X_prime)
+            pred: np.ndarray = self._predict_phi_matrix(
+                active_phi_matrix=phi_matrix)
+            return pred
+
+        def predict_y(self, X: np.ndarray) -> np.ndarray:
+            phi_matrix: np.ndarray = self._compute_phi_matrix(
+                X=X, X_prime=self._X_prime)
+            y: np.ndarray = phi_matrix @ self._mu
+            return y
+
+        @overrides
+        def _init_beta_matrix(self,
+                              target: np.ndarray) -> Tuple[np.ndarray, float]:
+            """Initialize beta matrix.
+
+            Args:
+                target (np.ndarray): (n_samples, ) The target vector.
+
+            Returns:
+                beta_matrix (np.ndarray): (n_samples, n_samples) The beta matrix.
+                init_beta (float): The initial beta value for self._init_alpha_matrix  function call
+            """
+            beta_diag = target.copy().astype(np.float)
+            beta_matrix: np.ndarray = np.diag(beta_diag)
+            init_beta: float = np.mean(beta_diag)
+            return beta_matrix, init_beta
+
+        @overrides
+        def _update_beta_matrix(self, active_alpha_matrix: np.ndarray,
+                                beta_matrix: np.ndarray,
+                                active_phi_matrix: np.ndarray,
+                                target: np.ndarray) -> np.ndarray:
+            """Update beta matrix.
+
+            Args:
+                active_phi_matrix (np.ndarray): (n_samples, n_active_basis_functions)
+
+            Returns:
+                np.ndarray: [description]
+            """
+            prob: np.ndarray = self._predict_phi_matrix(
+                active_phi_matrix=active_phi_matrix)
+            beta_diag: np.ndarray = prob * (1 - prob)
+            beta_matrix = np.diag(beta_diag)
+            return beta_matrix
+
+        @overrides
+        def _compute_target_hat(self, active_phi_matrix: np.ndarray,
+                                beta_matrix: np.ndarray,
+                                y: np.ndarray) -> np.ndarray:
+            """Compute target hat
+
+            Args:
+                X (np.ndarray): (n_samples, n_features) The input vector.
+                y (np.ndarray): (n_samples, )The ground truth target.
+
+            Returns:
+                target_hat (np.ndarray): (n_samples, ) The predicted target.
+            """
+            # re-assign to match paper expression
+            t: np.ndarray = y
+            y = self._predict_phi_matrix(active_phi_matrix=active_phi_matrix)
+            beta_inv: np.ndarray = np.linalg.inv(beta_matrix)
+            target_hat: np.ndarray = active_phi_matrix @ self._mu + beta_inv @ (
+                t - y)
+            return target_hat
+
+        # @overrides
+        # def _update_weight_posterior(
+        #         self, active_phi_matrix: np.ndarray,
+        #         active_alpha_matrix: np.ndarray, beta_matrix: np.ndarray,
+        #         target_hat: np.ndarray) -> Tuple[np.ndarray, np.ndarray]:
+        #     """Compute the "most probable" or "MP" weight posterior statistics
+
+        #     Args:
+        #         target_hat (np.ndarray): (n_samples, ) The target hat vector.
+        #         alpha_matrix_active (np.ndarray): (n_active_basis_vectors, n_active_basis_vectors) The current active alpha matrix.
+        #         beta_matrix (np.ndarray): (n_samples, n_samples) The beta matrix
+
+        #     Returns:
+        #         weight_posterior_mean (np.ndarray): (n_active_basis_vectors, )The updated weight posterior mean
+        #         weight_posterior_cov_matrix (np.ndarray): (n_active_basis_vectors, n_active_basis_vectors)
+        #     """
+        #     # force subclass to return so that the corresponding instance variables will definitely get updated.
+        #     phi_tr_beta: np.ndarray = active_phi_matrix.T @ beta_matrix
+        #     weight_posterior_cov_matrix: np.ndarray = np.linalg.inv(
+        #         phi_tr_beta @ active_phi_matrix + active_alpha_matrix)
+        #     weight_posterior_mean: np.ndarray = weight_posterior_cov_matrix @ phi_tr_beta @ target_hat
+        #     return weight_posterior_mean, weight_posterior_cov_matrix
+
+        def _predict_phi_matrix(self,
+                                active_phi_matrix: np.ndarray) -> np.ndarray:
+            """Predict training dataset with current active_phi_matrix.
+
+            Args:
+                active_phi_matrix (np.ndarray): (n_samples, n_active_basis) The current active phi matrix.
+
+            Returns:
+                pred (np.ndarray): (n_samples, ) The prediction of training dataset given current active phi matrix.
+            """
+            y: np.ndarray = active_phi_matrix @ self._mu
+            pred: np.ndarray = sigmoid(y)
+            return pred
 
     _kernel_func: Callable[[np.ndarray, np.ndarray], np.ndarray]
     _include_bias: bool
@@ -17,7 +136,7 @@ class RVC(ClassifierMixin, BaseEstimator):
     _max_iter: Union[int, None]
     _verbose: bool
 
-    _binary_rvc_list_: Union[List[_BinaryRVC], None]
+    _binary_rvc_list_: Union[List[__BinaryRVC], None]
     _n_classes_: Union[int, None]
 
     # _relevance_vectors_: Union[np.ndarray, None]  # aka self._X_prime
@@ -109,7 +228,7 @@ class RVC(ClassifierMixin, BaseEstimator):
         return self._verbose
 
     @property
-    def binary_rvc_list_(self) -> List[_BinaryRVC]:
+    def binary_rvc_list_(self) -> List[__BinaryRVC]:
         if self._binary_rvc_list_ is not None:
             return self._binary_rvc_list_
         raise ValueError("self._binary_rv_list_ is None")

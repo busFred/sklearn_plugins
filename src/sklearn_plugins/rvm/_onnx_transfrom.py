f1c704cbf601c58f625918496dd55d2b04e31e8a
busFred <32878707+busFred@users.noreply.github.com>
2021-08-18 16:04:27 -0400

    remove output y_var and add rvr_converter

--- src/sklearn_plugins/rvm/_onnx_transfrom.py
+++ src/sklearn_plugins/rvm/_onnx_transfrom.py
@@ -26,7 +26,8 @@ __copyright__ = "Copyright 2021, Hung-Tien Huang"
 
 class KernelFunction(ABC):
     @abstractmethod
-    def __call__(self, X, X_prime):
+    def __call__(self, X: Variable, X_prime: np.ndarray,
+                 op_version: Union[int, None]) -> OnnxOperator:
         pass
 
 
@@ -36,7 +37,8 @@ class RBFKernelFunction(KernelFunction):
     def __init__(self, gamma: Optional[float]) -> None:
         self.__gamma = gamma
 
-    def __compute_dist(self, X: Variable, X_prime: np.ndarray) -> OnnxOperator:
+    def __compute_dist(self, X: Variable, X_prime: np.ndarray,
+                       op_version: Union[int, None]) -> OnnxOperator:
         """
             For efficiency reasons, the euclidean distance between a pair of row
             vector x and y is computed as::
@@ -44,25 +46,31 @@ class RBFKernelFunction(KernelFunction):
                 dist(x, y) = sqrt(dot(x, x) - 2 * dot(x, y) + dot(y, y))
         """
         Y: np.ndarray = X_prime
-        X_tr: OnnxOperator = OnnxTranspose(X)
-        Y_tr: OnnxOperator = OnnxTranspose(Y)
-        X_tr_X: OnnxOperator = OnnxMatMul(X_tr, X)
-        Y_tr_Y: OnnxOperator = OnnxMatMul(Y_tr, Y)
-        X_tr_Y: OnnxOperator = OnnxMatMul(X_tr, Y)
-        two_X_tr_Y: OnnxOperator = OnnxMul(2.0, X_tr_Y)
-        dist: OnnxOperator = OnnxSum([X_tr_X, two_X_tr_Y, Y_tr_Y])
+        X_tr: OnnxOperator = OnnxTranspose(X, op_version=op_version)
+        Y_tr: OnnxOperator = OnnxTranspose(Y, op_version=op_version)
+        X_tr_X: OnnxOperator = OnnxMatMul(X_tr, X, op_version=op_version)
+        Y_tr_Y: OnnxOperator = OnnxMatMul(Y_tr, Y, op_version=op_version)
+        X_tr_Y: OnnxOperator = OnnxMatMul(X_tr, Y, op_version=op_version)
+        neg_two_X_tr_Y: OnnxOperator = OnnxMul(-2.0,
+                                               X_tr_Y,
+                                               op_version=op_version)
+        dist: OnnxOperator = OnnxSum([X_tr_X, neg_two_X_tr_Y, Y_tr_Y],
+                                     op_version=op_version)
         return dist
 
     @overrides
-    def __call__(self, X: Variable, X_prime: np.ndarray):
+    def __call__(self, X: Variable, X_prime: np.ndarray,
+                 op_version: Union[int, None]) -> OnnxOperator:
         gamma: float
         if self.__gamma is None:
             gamma = 1.0 / X.type.shape[1]
         else:
             gamma = self.__gamma
-        dist: OnnxOperator = self.__compute_dist(X, X_prime)
-        neg_gamma_dist: OnnxOperator = OnnxMul(-1.0 * gamma, dist)
-        rbf: OnnxOperator = OnnxExp(neg_gamma_dist)
+        dist: OnnxOperator = self.__compute_dist(X, X_prime, op_version)
+        neg_gamma_dist: OnnxOperator = OnnxMul(-1.0 * gamma,
+                                               dist,
+                                               op_version=op_version)
+        rbf: OnnxOperator = OnnxExp(neg_gamma_dist, op_version=op_version)
         return rbf
 
 
@@ -75,8 +83,10 @@ def rvr_shape_calculator(operator: Operator):
     if len(op_inputs) != 1:
         raise RuntimeError("Only one input matrix is allowed for RVR.")
     op_outputs: List[Variable] = operator.outputs
-    if len(op_outputs) != 2:
-        raise RuntimeError("Only two outputs are allowed for RVR.")
+    if len(op_outputs) != 1:
+        raise RuntimeError("Only one output is allowed for RVR.")
+    # if len(op_outputs) != 2:
+    #     raise RuntimeError("Only two outputs are allowed for RVR.")
     # retrieve rvr inputs dtype
     input_var_type: DataType = op_inputs[0].type
     # confirm rvr input and output shape
@@ -84,14 +94,24 @@ def rvr_shape_calculator(operator: Operator):
     # outputs[0], outputs[1] = rvr.predict_var(X)
     op_outputs[0].type.shape = [n_samples]
     op_outputs[0].onnx_name = "y"
-    op_outputs[1].type.shape = [n_samples]
-    op_outputs[1].onnx_name = "y_var"
+    # op_outputs[1].type.shape = [n_samples]
+    # op_outputs[1].onnx_name = "y_var"
 
 
 def rvr_converter(scope: Scope, operator: Operator,
-                  container: ModelComponentContainer):
-
-    pass
+                  container: ModelComponentContainer,
+                  kernel_func: KernelFunction):
+    rvr: RVR = operator.raw_operator
+    input: Variable = operator.inputs[0]
+    op_version: Union[int, None] = container.target_opset
+    op_outputs: List[Variable] = operator.outputs
+    phi_matrix: OnnxOperator = kernel_func(input, rvr.relevance_vectors_,
+                                           op_version)
+    y: OnnxOperator = OnnxMatMul(phi_matrix,
+                                 rvr.weight_posterior_mean_,
+                                 op_version=op_version,
+                                 output_names=[op_outputs[0]])
+    y.add_to(scope=scope, container=container)
 
 
 # def spherical_kmeans_converter(scope: Scope, operator: Operator,

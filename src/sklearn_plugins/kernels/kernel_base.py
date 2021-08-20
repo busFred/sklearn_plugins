78823696a2c4a63f67f6dcbddccb6dab07373467
busFred <32878707+busFred@users.noreply.github.com>
2021-08-20 15:10:58 -0400

    add kerenel_base

---
+++ src/sklearn_plugins/kernels/kernel_base.py
@@ -0,0 +1,20 @@
+from abc import ABC, abstractmethod
+from typing import Union
+
+import numpy as np
+from skl2onnx.algebra.onnx_operator import OnnxOperator
+from skl2onnx.common._topology import Variable
+
+
+class KernelFunction(ABC):
+    def __init__(self) -> None:
+        super().__init__()
+
+    @abstractmethod
+    def __call__(self, X: np.ndarray, X_prime: np.ndarray) -> np.ndarray:
+        pass
+
+    @abstractmethod
+    def convert_onnx(self, X: Variable, X_prime: np.ndarray,
+                     op_version: Union[int, None]) -> OnnxOperator:
+        pass

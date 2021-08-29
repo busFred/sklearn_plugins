ac79ae904e47d90b24b81b93239a45a10a5daeb3
busFred <32878707+busFred@users.noreply.github.com>
2021-08-29 11:50:17 -0400

    fix input output name conflict

--- src/sklearn_plugins/rvm/_onnx_transfrom.py
+++ src/sklearn_plugins/rvm/_onnx_transfrom.py
@@ -6,8 +6,9 @@ import numpy as np
 from onnxconverter_common.data_types import (DataType, DoubleTensorType,
                                              FloatTensorType, Int64TensorType)
 from skl2onnx.algebra.onnx_operator import OnnxOperator, OnnxSubEstimator
-from skl2onnx.algebra.onnx_ops import (OnnxArgMax, OnnxConcatFromSequence,
-                                       OnnxMatMul, OnnxPad, OnnxSoftmax)
+from skl2onnx.algebra.onnx_ops import (OnnxArgMax, OnnxConcat,
+                                       OnnxConcatFromSequence, OnnxMatMul,
+                                       OnnxPad, OnnxReshape, OnnxSoftmax)
 from skl2onnx.common._container import ModelComponentContainer
 from skl2onnx.common._topology import Operator, Scope, Variable
 from skl2onnx.common.data_types import guess_numpy_type
@@ -40,7 +41,7 @@ def rvr_shape_calculator(operator: Operator):
     n_samples: int = input_var_type.shape[0]
     # outputs[0], outputs[1] = rvr.predict_var(X)
     op_outputs[0].type.shape = [n_samples]
-    op_outputs[0].onnx_name = "y"
+    # op_outputs[0].set_onnx_name("y")
     # op_outputs[1].type.shape = [n_samples]
     # op_outputs[1].onnx_name = "y_var"
 
@@ -73,7 +74,7 @@ def rvc_shape_calculator(operator: Operator):
     check_input_and_output_types(
         operator,
         good_input_types=[FloatTensorType, DoubleTensorType],
-        good_output_types=[FloatTensorType, DoubleTensorType])
+        good_output_types=[FloatTensorType, DoubleTensorType, Int64TensorType])
     op_inputs: List[Variable] = operator.inputs
     if len(op_inputs) != 1:
         raise RuntimeError("Only one input matrix is allowed for RVC.")
@@ -86,15 +87,19 @@ def rvc_shape_calculator(operator: Operator):
     # confirm rvc input and output shape
     n_samples: int = input_var_type.shape[0]
     # outputs[0] = rvc.predict(X)
-    op_outputs[0].type = Int64TensorType(shape=[n_samples])
-    op_outputs[0].onnx_name = "labels"
+    op_outputs[0].set_type(Int64TensorType(shape=[n_samples]))
+    # op_outputs[0].set_onnx_name("labels")
+    # op_outputs[0].type = Int64TensorType(shape=[n_samples])
+    # op_outputs[0].onnx_name = "labels"
     # outputs[1] = rvc.predict_proba(X)
     rvc: RVC = operator.raw_operator
     n_classes: int = rvc.n_classes_
     ModelTensorType: Union[Type[FloatTensorType],
                            Type[DoubleTensorType]] = input_var_type.__class__
-    op_outputs[1].type = ModelTensorType(shape=[n_samples, n_classes])
-    op_outputs[1].onnx_name = "probabilities"
+    op_outputs[1].set_type(ModelTensorType(shape=[n_samples, n_classes]))
+    # op_outputs[1].set_onnx_name("probabilities")
+    # op_outputs[1].type = ModelTensorType(shape=[n_samples, n_classes])
+    # op_outputs[1].onnx_name = "probabilities"
 
 
 def rvc_converter(scope: Scope, operator: Operator,
@@ -104,14 +109,12 @@ def rvc_converter(scope: Scope, operator: Operator,
     input: Variable = operator.inputs[0]
     op_outputs: List[Variable] = operator.outputs
     op_version: Union[int, None] = container.target_opset
-    y_list: List[OnnxSubEstimator] = [
-        OnnxSubEstimator(bsvc, input, op_version=op_version)
-        for bsvc in rvc.binary_rvc_list_
+    y_list: List[OnnxOperator] = [
+        OnnxReshape(OnnxSubEstimator(bsvc, input, op_version=op_version),
+                    np.array([-1, 1], dtype=np.int64),
+                    op_version=op_version) for bsvc in rvc.binary_rvc_list_
     ]
-    y_matrix: OnnxOperator = OnnxConcatFromSequence(y_list,
-                                                    axis=1,
-                                                    newaxis=1,
-                                                    op_version=op_version)
+    y_matrix: OnnxOperator = OnnxConcat(*y_list, axis=1, op_version=op_version)
     probs: OnnxOperator = OnnxSoftmax(y_matrix,
                                       axis=1,
                                       op_version=op_version,
